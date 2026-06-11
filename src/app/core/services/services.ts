b587548d96fcc05bd3b6b322ca847a76a2ae5ce3
valncabs <cabasmirandavalen@gmail.com>
2026-06-11 13:17:25 -0500

    .

--- src/app/core/services/services.ts
+++ src/app/core/services/services.ts
@@ -3,7 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-services',
   imports: [],
-  templateUrl: './services.html',
-  styleUrl: './services.css',
+  templateUrl: './services.html'
 })
 export class Services {}
