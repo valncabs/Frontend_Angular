96394ae9005a64de39078cfa375219eb6d3ba88b
Juan <jmigueldemoya@unibarranquilla.edu.co>
2026-07-07 15:58:30 -0500

    Juan

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,21 +7,5 @@ import { provideAuth0 } from '@auth0/auth0-angular';
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes),
-
-    provideHttpClient(withInterceptorsFromDi()),
-
-    provideAuth0({
-      domain: 'dev-ukkb01mu1aknyu4h.us.auth0.com',
-      clientId: 'FL9OxuiPAHqgqxmPevNOshZcE4vovqnp',
-
-      authorizationParams: {
-        redirect_uri: window.location.origin,
-      },
-
-      cacheLocation: 'localstorage',
-      useRefreshTokens: true,
-    }),
-  ],
+  providers: [provideRouter(routes), provideHttpClient(withInterceptorsFromDi())],
 };
