e1221ae04709e9633512ea6a7397b639c3612473
Juan <jmigueldemoya@unibarranquilla.edu.co>
2026-07-07 16:03:10 -0500

    Juan

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,8 +2,6 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { routes } from './app.routes';
 
-import { provideAuth0 } from '@auth0/auth0-angular';
-
 import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
