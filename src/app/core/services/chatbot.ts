8a0afffa233b8e2ef58295d35b6ef58960811e9d
Juan <jmigueldemoya@unibarranquilla.edu.co>
2026-07-21 14:58:38 -0500

    redireccion del bot a host

--- src/app/core/services/chatbot.ts
+++ src/app/core/services/chatbot.ts
@@ -2,23 +2,18 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-
 export interface Fuente {
-
   documento: string;
 
   pagina: number;
 
   fragmento: string;
-
 }
 
 export interface ChatResponse {
-
   answer: string;
 
   sources: Fuente[];
-
 }
 
 /* ===========================
@@ -26,25 +21,16 @@ export interface ChatResponse {
 =========================== */
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ChatbotService {
-
   private readonly api = 'https://rag-neww.onrender.com/ask';
 
-  constructor(
-    private http: HttpClient
-  ) {}
+  constructor(private http: HttpClient) {}
 
   ask(question: string): Observable<ChatResponse> {
-
-    return this.http.post<ChatResponse>(
-      this.api,
-      {
-        question
-      }
-    );
-
+    return this.http.post<ChatResponse>(this.api, {
+      question,
+    });
   }
-
 }
